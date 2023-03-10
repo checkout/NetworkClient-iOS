ec2e286387a37320b52331edb52657ac622b352a
Alex Ioja-Yang <alex.ioja-yang@checkout.com>
2023-03-10 09:56:33 +0000

    Provide platform in Package declaration

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "CheckoutNetwork",
+    platforms: [
+        .iOS(.v11),
+    ],
     products: [
         .library(
             name: "CheckoutNetwork",
